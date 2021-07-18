a63291d762b0c64f71eda29095a6bfa09667b60b
luoxiaoluo <857396489@qq.com>
2021-07-18 10:36:16 +0800

    尝试解决冲突

--- jQuery_ajax/src/com/imooc/ajax/AjaxHttpServlet.java
+++ jQuery_ajax/src/com/imooc/ajax/AjaxHttpServlet.java
@@ -12,6 +12,7 @@ public class AjaxHttpServlet extends HttpServlet{
 //	¾ÍÐÞ¸ÄÏÂ£¬²âÊÔgit¡£
 //	once again
 // 	server change
+//	conflict client
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.getWriter().print("<b> I'm Server Content</b>");
